25c05c5d66ccb21e369a2cbfae377a3e661d22f7
xBlank77 <127398500+xBlank77@users.noreply.github.com>
2024-03-14 16:16:12 +0800

    Add project files.

---
+++ ToDoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+
+namespace ToDoList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {   
+        }
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
---
+++ ToDoList/Models/ToDo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class ToDo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Date/Time")]
+        public string? IsDeleted { get; set; }
+        public string? Description { get; set; }
+
+    }
+
+}
